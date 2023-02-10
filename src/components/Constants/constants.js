3cd27f9070d31756d83221aed67d9252c9263a0b
amalrajsr <sramalraj123@gmail.com>
2023-02-10 22:31:43 +0530

    .env removed

--- src/components/Constants/constants.js
+++ src/components/Constants/constants.js
@@ -1,6 +1,6 @@
 
 const BASEURL= 'https://api.themoviedb.org/3'
-const API_KEY=process.env.REACT_APP_API_KEY
+const API_KEY= process.env.REACT_APP_API_KEY
 const IMAGE_URL='https://image.tmdb.org/t/p/original'
 
 module.exports={
